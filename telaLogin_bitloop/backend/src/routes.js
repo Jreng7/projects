27ab7a373f3ba0974b89cfd2da96a71f49d4b6b1
Jreng7 <jr.waydev@gmail.com>
2025-02-03 18:00:37 -0300

    routes finalizado

--- telaLogin_bitloop/backend/src/routes.js
+++ telaLogin_bitloop/backend/src/routes.js
@@ -2,6 +2,24 @@ import { Router } from 'express'
 
 export const router = Router()
 
+const users = [{ 
+
+  id: 1, 
+  name: 'John Doe', 
+  email: 'contato@johndoe.com',
+  password: '12347'
+
+}]
+
 router.post('/login', (req, res) => {
-  return res.send('Login endpoint')
+
+  const { email, password } = req.body
+
+  const user = users.find(user => user.email === email && user.password === password)
+
+  if( user ) {
+    return res.status(200).json(user)
+  }
+
+  return res.status(401).json({ message: 'Invalid credentials'})
 })  
