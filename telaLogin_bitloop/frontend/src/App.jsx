ee39793ab83b5cb7b462e9398dbe6b862db06019
Jreng7 <jr.waydev@gmail.com>
2025-02-03 18:10:11 -0300

    estruturando css

---
+++ telaLogin_bitloop/frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <main>
+        <div className="login-form-wrap">
+          <h2>Login</h2>
+
+          <form action="">
+
+          </form>
+        </div>
+     </main>
+    </>
+  )
+}
+
+export default App
