67cb57697f667b38ff05fac0aed487a3e8567d6d
Jreng7 <jr.waydev@gmail.com>
2025-03-09 00:21:14 -0300

    importando o jsonwebtoken

--- gerenciamento_repositorios/src/middlewares/auth.js
+++ gerenciamento_repositorios/src/middlewares/auth.js
@@ -1,9 +1,11 @@
-
+import jwt from 'jsonwebtoken'
 
 export default async (req, res, next) => {
   const authenticated = false
 
   if (authenticated) {
     return next()
+  } else {
+    return res.status(401).end()
   }
 }
