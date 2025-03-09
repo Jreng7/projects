9724ebef09f9e7f3ade9626720c4fe93033ef653
Jreng7 <jr.waydev@gmail.com>
2025-03-09 00:51:43 -0300

    desestruturando o authHeader.headers.authorization com "const parts = authHeader.split('  ')

--- gerenciamento_repositorios/src/middlewares/auth.js
+++ gerenciamento_repositorios/src/middlewares/auth.js
@@ -9,6 +9,14 @@ export default async (req, res, next) => {
     return res.status(401).json({ error: 'Token was not provided' })
   }
 
+  const parts = authHeader.spli(' ')
+
+  if (parts.length !== 2 || parts[0] !== "Bearer") {
+    return res.status(401).json({ error: "Formato de token inválido." })
+  }
+
+  const [, token] = parts
+
   const authenticated = true
 
   if (authenticated) {
