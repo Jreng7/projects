d7dae68336217b963f917c951642fd1b34430fcd
Jreng7 <jr.waydev@gmail.com>
2025-03-14 16:40:41 -0300

    chegando User.js e auth.js

--- gerenciamento_repositorios/src/models/User.js
+++ gerenciamento_repositorios/src/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
 
   {
     email: { type: String, required: true, index: {unique: true} },
-    password: { type: String, required: true }
+    password: { type: String, required: true, select: false }
   },
 
   // CONFIG do Model. Preenchido automaticamente pelo mongoDB
--- gerenciamento_repositorios/src/services/auth.js
+++ gerenciamento_repositorios/src/services/auth.js
@@ -1,7 +1,7 @@
 import { hash, compare } from "bcryptjs";
 
-export const createPasswordHash = async (parametro) => {
-  return await hash(parametro, 10)
+export const createPasswordHash = async (senha) => {
+  return await hash(senha, 10)
 }
 
 export const checkPassword = async (user, password) => {
