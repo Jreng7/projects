88a2439e523d973fda68b3b6c42b6af11e989b07
Jreng7 <jr.waydev@gmail.com>
2025-03-20 12:13:03 -0300

    authMiddleware e LoginController.login

--- gerenciamento_repositorios/src/routes/repositories.routes.js
+++ gerenciamento_repositorios/src/routes/repositories.routes.js
@@ -1,7 +1,14 @@
 import { Router } from "express"
 import RepositoriesController from "../controllers/Repository.Controller.js"
+import { loginController } from '../controllers/Session.Controller'
+import { authMiddleware } from '../middlewares/auth.login.js'
+
 
 const repositoryRouter = Router()
+
+repositoryRouter.put('login', loginController.login)
+
+repositoryRouter.use(authMiddleware)
 
 repositoryRouter.get('/', RepositoriesController.index)
 repositoryRouter.post('/', RepositoriesController.create);
--- gerenciamento_repositorios/src/routes/users.routes.js
+++ gerenciamento_repositorios/src/routes/users.routes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express'
 import UsersController from '../controllers/User.Controller.js'
+import { loginController } from '../controllers/Session.Controller.js'
+import { authMiddleware } from '../middlewares/auth.login.js'
 
 export const usersRouter = Router()
+
+usersRouter.put('/login', loginController.login)
+
+usersRouter.use(authMiddleware)
 
 usersRouter.get('/', UsersController.index)
 usersRouter.get('/:id', UsersController.show)
