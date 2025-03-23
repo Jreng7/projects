c057fa8a33e26e7ba325540963bf997f4ccda513
Jreng7 <jr.waydev@gmail.com>
2025-03-22 21:32:38 -0300

    editando boxform que engloba todo conteudo

--- gerenciamento_repositorios/frontend/src/pages/LoginPage/index.jsx
+++ gerenciamento_repositorios/frontend/src/pages/LoginPage/index.jsx
@@ -1,27 +1,30 @@
 import React from "react";
+import './styles.css';
+
 
 const LoginPage = () => {
   return (
     <div id="login">
+    
+      <div className="boxform">
+        <h1 className="title">Login</h1>
+        <form className="form">
 
-      <h1 className="title">Login</h1>
-      <form className="form">
+          <div className="field">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" />
+          </div>
 
-        <div className="field">
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" />
-        </div>
+          <div className="field">
+            <label htmlFor="password">Senha:</label>
+            <input type="password" name="password" id="password" />
+          </div>
 
-        <div className="field">
-          <label htmlFor="password">Senha:</label>
-          <input type="password" name="password" id="password" />
-        </div>
-
-        <div className="actions">
-          <button>Entrar</button>
-        </div>
-        
-      </form>
+          <div className="actions">
+            <button>Entrar</button>
+          </div>
+        </form>
+      </div>
 
     </div>
   )
