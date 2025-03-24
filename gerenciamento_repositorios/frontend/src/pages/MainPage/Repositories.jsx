a0c197d4f85b2d3ef77e489477bfd5269ac46f4d
Jreng7 <jr.waydev@gmail.com>
2025-03-24 17:52:55 -0300

    refactor: (MainPage) criando novas funções para newRepo

---
+++ gerenciamento_repositorios/frontend/src/pages/MainPage/Repositories.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+export function Repositories({ repo, deleteRepo, onNewRepo }){
+
+  const [ newRepo, setNewRepo ] = useState('')
+
+  return (
+    <>
+       <div className="repositories">
+        <h2>Repositórios</h2>
+
+        <ul>
+          <li>
+            <p>
+              <div className="owner">Facebook</div>
+              <div className="name">React</div>
+            </p>
+            <button onClick={() => deleteRepo(null)}>x</button>
+          </li>
+        </ul>
+ 
+        <div className="search">
+          <label htmlFor="repo" className="label-repo">Novo Repositório:</label>
+          <input type="url" id="repo" name="repo" value={newRepo} onChange={ (e) => setNewRepo(e.target.value) }/>
+          <div className="btn">
+            <button onClick={() => onNewRepo(newRepo)}>Adicionar</button>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
--- gerenciamento_repositorios/frontend/src/pages/MainPage/index.jsx
+++ gerenciamento_repositorios/frontend/src/pages/MainPage/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Nav } from "./Nav";
 import { Search } from "./Search";
+import { Repositories } from "./Repositories";
 import "./style.css"
 
 const MainPage = () => {
@@ -19,40 +20,9 @@ const MainPage = () => {
 
   return (
     <div className="mainpage">
-
       <Nav argumento={handleLogout}/>
       <Search search={handleSearch}/>
-      
-      <div className="repositories">
-        <h2>Repositórios</h2>
-
-        <ul>
-          <li>
-            <p>
-              <div className="owner">Facebook</div>
-              <div className="name">React</div>
-            </p>
-            <button onClick={handleDeleteRepo}>x</button>
-          </li>
-
-          <li>
-            <p>
-              <div className="owner">Facebook</div> 
-              <div className="name">React</div>
-            </p>
-            <button>x</button>
-          </li>
-        </ul>
- 
-        <div className="search">
-          <label htmlFor="repo" className="label-repo">Novo Repositório:</label>
-          <input type="url" id="repo" name="repo"/>
-          <div className="btn">
-            <button onClick={handleSearch}>Adicionar</button>
-          </div>
-        </div>
-
-      </div>
+      <Repositories />
     </div>
   )
 }
