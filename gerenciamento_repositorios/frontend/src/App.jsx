d3ba6b95565fcfecda1c7d7a895c1eb8080ba341
Jreng7 <jr.waydev@gmail.com>
2025-03-24 10:13:54 -0300

    style: Ajuster no App.jsx para ser altura necessária

--- gerenciamento_repositorios/frontend/src/App.jsx
+++ gerenciamento_repositorios/frontend/src/App.jsx
@@ -3,10 +3,10 @@ import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <AppRoutes />
       <Footer />
-    </>
+    </div>
   )
 }
 
